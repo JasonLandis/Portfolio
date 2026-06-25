3883891b083cf4e952e2ef4b1b17d58446fc24ce
Jason Landis <jasonelandis@gmail.com>
2026-06-24 23:36:54 -0400

    checkpoint

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -7,4 +7,34 @@ import { CommonModule } from '@angular/common';
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
-export class HomeComponent {}
+export class HomeComponent {
+  rows = 30;
+  cols = 30;
+
+  tiles: { row: number, col: number, active: boolean }[][] = Array.from({ length: this.rows }, (_, row) =>
+    Array.from({ length: this.cols }, (_, col) => ({
+      row,
+      col,
+      active: false
+    }))
+  );
+
+  ripple(clickedRow: number, clickedCol: number) {
+    this.tiles.flat().forEach(tile => {
+
+      const distance = Math.sqrt(
+        Math.pow(tile.row - clickedRow, 2) +
+        Math.pow(tile.col - clickedCol, 2)
+      );
+
+      setTimeout(() => {
+        tile.active = true;
+
+        setTimeout(() => {
+          tile.active = false;
+        }, 300);
+
+      }, distance * 50);
+    });
+  }
+}
